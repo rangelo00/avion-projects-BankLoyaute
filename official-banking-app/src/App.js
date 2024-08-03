47fb02f9a24e3a6eab192c68b7d60764540f79d9
Shem Bernardino <shem.nicole.bernardino@gmail.com>
2024-08-03 11:32:10 +0800

    Added navBar

--- official-banking-app/src/App.js
+++ official-banking-app/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import bankLogo from './assets/images/bank_loyaute_cropped.png'
+import navBar from './components/navBar/navBar'
 
 function App() {
   return (
     <div className="App">
       <img src={bankLogo} alt="bankLogo"></img>
+      <navBar></navBar>
     </div>
   );
 }
---
+++ official-banking-app/src/components/navBar/navBar.jsx
@@ -0,0 +1,12 @@
+import './navBar.css'
+
+function navBar() {
+    return (
+        <div className="navBar">
+            <input type="text" placeholder="Search"></input>
+            <button className="btn">Search</button>
+        </div>
+    )
+}
+
+export default navBar;
