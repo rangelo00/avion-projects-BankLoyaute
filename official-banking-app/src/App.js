4160059f8b17d0b680d54637f2ed834ad2eb9ca5
Shem Bernardino <shem.nicole.bernardino@gmail.com>
2024-08-03 11:38:14 +0800

    Fixed warnings for NavBar

--- official-banking-app/src/App.js
+++ official-banking-app/src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 import bankLogo from './assets/images/bank_loyaute_cropped.png'
-import navBar from './components/navBar/navBar'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
       <img src={bankLogo} alt="bankLogo"></img>
-      <navBar></navBar>
+      <NavBar></NavBar>
     </div>
   );
 }
--- official-banking-app/src/components/navBar/navBar.jsx
+++ official-banking-app/src/components/navBar/navBar.jsx
@@ -1,8 +1,8 @@
-import './navBar.css'
+import './NavBar.css'
 
 function navBar() {
     return (
-        <div className="navBar">
+        <div className="NavBar">
             <input type="text" placeholder="Search"></input>
             <button className="btn">Search</button>
         </div>
